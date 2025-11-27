05d449c0915d8308e9898771d7e350d158ef615f
Naresh Gantala <nareshgantala@gmail.com>
2025-11-27 13:50:05 +0530

    up

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -42,7 +42,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
